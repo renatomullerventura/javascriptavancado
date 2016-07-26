b7124bee0905a0ab18f1b799735663672daa2ab3
Renato Mulller Ventura <re.infotec@gmail.om>
2016-07-26 13:16:42 +0000

    6° aula, adicionando botões de ação para alterar os itens

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ function getTotal(list){
 function setList(list){
     var table = '<thead><tr><td>Description</td><td>Amount</td><td>Value</td><td>Action</td></tr></thead><tbody>';
     for (var key in list) {
-        table += '<tr><td>'+formatDesc(list[key].desc)+'</td><td>'+list[key].amount+'</td><td>'+formatValue(list[key].value)+'</td><td>Edit | Delete</td></tr>';
+        table += '<tr><td>'+formatDesc(list[key].desc)+'</td><td>'+list[key].amount+'</td><td>'+formatValue(list[key].value)+'</td><td><button onclick="setUpdate('+key+')" class="btn btn-default">Edit</button> <button onclick="setUpdate('+key+')" class="btn btn-default">Delete</button></td></tr>';
     }
     table+='</tbody>';
     document.getElementById("listTable").innerHTML = table;
@@ -43,6 +43,24 @@ function addData(){
     list.unshift({'desc':desc, 'amount':amount, 'value':value});
     setList(list);
     
+}
+
+function setUpdate(id){
+    
+    var obj =   list[id];
+    document.getElementById('desc').value   =   obj.desc;
+    document.getElementById('amount').value   =   obj.amount;
+    document.getElementById('value').value   =   obj.value;
+    document.getElementById('btnUpdate').style.display  =   "inline-block";
+    document.getElementById('btnAdd').style.display  =   "none";
+}
+
+function resetForm(){
+    document.getElementById('desc').value   =   '';
+    document.getElementById('amount').value   =   '';
+    document.getElementById('value').value   =   '';
+    document.getElementById('btnUpdate').style.display  =   "none";
+    document.getElementById('btnAdd').style.display  =   "inline-block";
 }
 
 setList(list);
