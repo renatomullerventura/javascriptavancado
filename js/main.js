3e1e43ec442da9e6190d9c0e3f8920d632fcea6d
Renato Mulller Ventura <re.infotec@gmail.om>
2016-07-27 11:45:48 +0000

    9° aula, validações

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ function getTotal(list){
 function setList(list){
     var table = '<thead><tr><td>Description</td><td>Amount</td><td>Value</td><td>Action</td></tr></thead><tbody>';
     for (var key in list) {
-        table += '<tr><td>'+formatDesc(list[key].desc)+'</td><td>'+list[key].amount+'</td><td>'+formatValue(list[key].value)+'</td><td><button onclick="setUpdate('+key+')" class="btn btn-default">Edit</button> <button onclick="deleteData('+key+')" class="btn btn-default">Delete</button></td></tr>';
+        table += '<tr><td>'+formatDesc(list[key].desc)+'</td><td>'+formatAmount(list[key].amount)+'</td><td>'+formatValue(list[key].value)+'</td><td><button onclick="setUpdate('+key+')" class="btn btn-default">Edit</button> <button onclick="deleteData('+key+')" class="btn btn-default">Delete</button></td></tr>';
     }
     table+='</tbody>';
     document.getElementById("listTable").innerHTML = table;
@@ -24,24 +24,33 @@ function setList(list){
 
 function formatDesc(desc){
     var str =   desc.toLowerCase();
-    str =   str.charAt(0).toUpperCase() + str.slice(1);
+    str     =   str.charAt(0).toUpperCase() + str.slice(1);
     return str;
 }
 
 function formatValue(value){
     var str =   parseFloat(value).toFixed(2) + "";
-    str =   str.replace('.',',');
-    str =   '$' + str;
+    str     =   str.replace('.',',');
+    str     =   '$' + str;
     return str;
 }
 
+function formatAmount(amount){
+    return parseInt(amount);
+}
+
 function addData(){
+    
+    if(!validation()){
+        return;
+    }
     var desc    =   document.getElementById("desc").value;
-    var amount    =   document.getElementById("amount").value;
-    var value    =   document.getElementById("value").value;
+    var amount  =   document.getElementById("amount").value;
+    var value   =   document.getElementById("value").value;
     
     list.unshift({'desc':desc, 'amount':amount, 'value':value});
     setList(list);
+    resetForm();
     
 }
 
@@ -64,14 +73,20 @@ function resetForm(){
     document.getElementById('btnUpdate').style.display  =   "none";
     document.getElementById('btnAdd').style.display  =   "inline-block";
     document.getElementById('inputIDUpdate').innerHTML = '';
+    document.getElementById('errors').style.display =   'none';
 }
 
 function updateData(){
-    var id = document.getElementById('idUpdate').value;
-    var desc = document.getElementById('desc').value;
-    var amount = document.getElementById('amount').value;
-    var value = document.getElementById('value').value;
-    list[id] = {'desc':desc, 'amount':amount, 'value':value };
+    
+    if(!validation()){
+        return;
+    }
+    
+    var id      = document.getElementById('idUpdate').value;
+    var desc    = document.getElementById('desc').value;
+    var amount  = document.getElementById('amount').value;
+    var value   = document.getElementById('value').value;
+    list[id]    = {'desc':desc, 'amount':amount, 'value':value };
     resetForm();
     setList(list);
 }
@@ -89,6 +104,42 @@ function deleteData(id){
         setList(list);
         
     }
+}
+
+function validation(){
+    var desc = document.getElementById('desc').value;
+    var amount = document.getElementById('amount').value;
+    var value = document.getElementById('value').value;
+    var errors  =   '';
+    document.getElementById('errors').style.display =   'none';
+    
+    if (desc === '') {
+        errors += '<p>Fill out description</p>';
+    }
+    if (amount === '') {
+        errors += '<p>Fill out amount</p>';
+    }else if(amount != parseInt(amount)){
+        errors += '<p>Fill out a valid amount</p>';
+    }
+    if (value === '') {
+        errors += '<p>Fill out value</p>';
+    }else if(value != parseFloat(value)){
+        errors += '<p>Fill out a valid value</p>';
+    }
+    
+    if(errors != ''){
+        document.getElementById('errors').style.display =   'block';
+        document.getElementById('errors').style.backgroundColor = 'rgba(85,85,85,0.3)';
+        document.getElementById('errors').style.color   =   'white';
+        document.getElementById('errors').style.padding =   '10px';
+        document.getElementById('errors').style.margin  =   '10px'
+        document.getElementById('errors').style.borderRadius    =   '13px';
+        document.getElementById('errors').innerHTML =   '<h3>Error: </h3>'+errors;
+        return 0;
+    }else{
+        return 1;
+    }
+    
 }
 
 setList(list);
